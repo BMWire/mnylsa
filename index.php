881fad55a4fc67ab9c3f0ae5c1f8f15b327ff45c
BMWire <bmwemawire@gmail.com>
2022-04-16 22:58:37 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["user_id"])) {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = "SELECT * FROM user
+            WHERE id = {$_SESSION["user_id"]}";
+    
+    // Check to see if the field isAdmin !== NULL
+    if($_SESSION["isAdmin"] !== NULL) {
+        $sql = "SELECT * FROM user
+                WHERE id = {$_SESSION["user_id"]}
+                AND isAdmin = 1";
+    }
+
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+    
+    <h1>Home</h1>
+    
+    <?php if (isset($user)): ?>
+        
+        <p>Hello <?= htmlspecialchars($user["name"]) ?></p>
+
+        <?php
+            if ($user["isAdmin"] === 1) {
+                echo "<p>You are an admin</p>";
+            }
+        ?>
+        
+        <p><a href="logout.php">Log out</a></p>
+        
+    <?php else: ?>
+        
+        <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
+        
+    <?php endif; ?>
+    
+</body>
+</html>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
