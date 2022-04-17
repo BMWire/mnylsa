1c1647520c440e51328015c3089e999f0b371764
BMWire <bmwemawire@gmail.com>
2022-04-17 18:53:07 +0300

    chore: merge home screen, navbar with styles from moneylisa

---
+++ home.php
@@ -0,0 +1,52 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["user_id"])) {
+    // create a database connection 
+    $mysqli = require __DIR__ . "/database.php";
+
+    // get the user's email address
+    $sql = "SELECT * FROM users
+            WHERE id = {$_SESSION["user_id"]}";
+
+    $result = $mysqli->query($sql);
+
+    $user = $result->fetch_assoc();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Home</title>
+
+    <!-- Styling imports -->
+    <link rel='stylesheet' href='styles/main.css'>
+    <link rel='stylesheet' href='styles/utility.css'>
+
+    <!-- Bootstrap Icons imports -->
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css'>
+
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"> -->
+</head>
+
+<body>
+    <?php include __DIR__ . "/navbar.php"; ?>
+
+    <!-- Start of main -->
+    <main>
+
+        <h1>Home</h1>
+
+    </main>
+    <!-- End of main -->
+
+
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,98 @@
+<!-- Start of navigation -->
+
+<!-- Start of md navigation -->
+<nav class='navbar mobile-hide'>
+    <div class='container-fluid'>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+                <a href='home.php' class='nav-link'>Artists</a>
+            </li>
+        </ul>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+                <a href='home.php' class='nav-link'>Galleries</a>
+            </li>
+        </ul>
+        <a href='home.php' class='nav-header mr-5'>
+            MONEYLISA
+        </a>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+                <a href='home.php' class='nav-link'>Trending</a>
+            </li>
+        </ul>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+                <!-- Check to see if there is a user session -->
+                <?php if (isset($user)) : ?>
+                    <p>
+                        Hey
+                        <a href='client-dashboard.php'>
+                            <?= substr($user['name'], 0, strpos($user['name'], ' ')) ?>
+                        </a>
+                        |
+                        <a href='logout.php' class='space-cadet'>Log out</a>
+                    </p>
+
+                    <p></p>
+
+                <?php else : ?>
+
+                    <div class='col-lg-12'>
+                        <p><a href='login.php'>Sign In</a> or <a href='signup.html'>Sign Up</a></p>
+                    </div>
+
+                <?php endif; ?>
+            </li>
+        </ul>
+    </div>
+</nav>
+<hr class='mobile-hide hr' />
+<!-- End of md navigation -->
+
+<!-- Start of mobile navigation -->
+<div class='mobile-show'>
+    <div class='container'>
+        <a href='home.php' class='nav-header center'>
+            <center>MONEYLISA</center>
+        </a>
+        <hr class='hr-mobile' />
+
+        <ul class='nav justify-content-center'>
+            <li class='nav-item'>
+                <a href='home.php' class='nav-link'>Galleries</a>
+            </li>
+            <!-- <li class='nav-item'>
+                <a href='/home.php' class='nav-link'>Artists</a>
+            </li> -->
+            <!-- <li class='nav-item'>
+                <a href='/home.php' class='nav-link'>Trending</a>
+            </li> -->
+            <!-- Check to see if there is a user session -->
+            <?php if (isset($user)) : ?>
+                <li class='nav-item'>
+                    <a href='client-dashboard.php' class='nav-link imperial-red'>
+                         <?= substr($user['name'], 0, strpos($user['name'], ' ')) ?>
+                    </a>
+                    <a href='logout.php' class='nav-link space-cadet'>Log out</a>
+                </li>
+                <li class='nav-item'>
+                <?php else : ?>
+
+                    <p class='nav-link'>
+                        <a href='login.php'>Sign In</a>
+                        or
+                        <a href='signup.html'>Sign Up</a>
+                    </p>
+                </li>
+
+            <?php endif; ?>
+        </ul>
+        <hr class='hr-mobile' />
+    </div>
+</div>
+
+<!-- End of mobile navigation -->
+
+<!-- End of navigation -->
+<!-- -------------------------------------------------------------------------------------------- -->
