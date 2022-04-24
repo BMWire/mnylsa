d8d33a520afef65e8ac701d8095be2663f84e2a1
BMWire <bmwemawire@gmail.com>
2022-04-24 16:48:57 +0300

    chore: R in CRUD for artist-galleries

--- artist-galleries.php
+++ artist-galleries.php
@@ -100,7 +100,68 @@ if (isset($_SESSION['user_id'])) {
                 </div>
                 <hr class='mobile-hide hr' />
 
+                <!-- Fetch all the gallery record entries by the artist who is logged in -->
+                <!-- Render a card for each gallery -->
+                <?php
+                $mysqli = require __DIR__ . '/database.php';
 
+                $fetch_stmt = "SELECT * FROM galleries WHERE artist_id = {$_SESSION['user_id']}";
+
+                $result = $mysqli->query($fetch_stmt);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $gallery_id = $row['id'];
+                        $gallery_name = $row['title'];
+                        $gallery_location = $row['location'];
+                        $gallery_story = $row['story'];
+                        $gallery_fee = $row['fee'];
+                        $gallery_createdAt = $row['created_at'];
+                        $gallery_imgDir = $row['coverImg'];
+                ?>
+                        <div class='row mt-5 justify-content-center'>
+                            <div class='col-lg-8 col-md-6'>
+                                <div class='card mb-3'>
+                                    <div class='row'>
+                                        <div class='col-md-4'>
+                                            <img src='<?= $gallery_imgDir ?>' class='card-img-top' alt='<? $gallery_name ?>'>
+                                        </div>
+                                        <div class='col-md-8 px-5'>
+                                            <div class='card-body'>
+                                                <h3 class='space-cadet cadet-underlined'><?= $gallery_name ?></h3>
+                                                <p class='card-text'>
+                                                    <i class='bi bi-geo-fill manatee' style='font-size:22px;'></i>
+                                                    <?= $gallery_location ?>
+                                                </p>
+                                                <p class='card-text'>
+                                                    <i class='bi bi-lightbulb-fill manatee' style='font-size:22px;'></i>
+                                                    <?= $gallery_story ?>
+                                                </p>
+                                                <p class='card-text'>
+                                                    <i class='bi bi-cash-coin manatee' style='font-size:22px;'></i>
+                                                    Kshs <?= $gallery_fee ?>
+                                                </p>
+                                                <p class='card-text'>
+                                                    <i class='bi bi-calendar-date manatee' style='font-size:22px;'></i>
+                                                    <?= $gallery_createdAt ?>
+                                                </p>
+                                                <center>
+                                                    <a href='artist-gallery-details.php?id=<?= $gallery_id ?>' class='btn btn-imperial'>
+                                                        View gallery
+                                                    </a>
+                                                </center>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                <?php
+                    }
+                } else {
+                    echo "<center class='fs-5 mt-6 imperial-red'>No galleries found. Try uploading one. </center>";
+                }
+                ?>
 
             </main>
             <!-- End of main -->
