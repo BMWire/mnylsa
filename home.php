bc2bd892a73b74cafa07e2f1ad53ce3a805ea24b
BMWire <bmwemawire@gmail.com>
2022-04-24 17:57:14 +0300

    chore: R in CRUD home page

--- home.php
+++ home.php
@@ -49,108 +49,68 @@ if (isset($_SESSION["user_id"])) {
     <!-- Start of main -->
     <main>
         <div class='container'>
-            
-            <?php if (isset($user)) : ?>
-                My name is <?= $user['name'] ?>.
-                My user id is <?= $user['id'] ?>.
-            <?php endif; ?>
-
             <div class='row'>
-                <div class='col-lg-4 col-md-6 col-sm-12 px-3 py-4'>
-                    <div class='card'>
-                        <img src='assets/art/american-gothic-grant.webp' class='card-img-top' alt='American Gothic Grant Wood'>
-                        <div class='card-body'>
-                            <div class='row ml-1'>
-                                <div class='col-5'>
-                                    <a href='/art-page.php'>
-                                        <h4 class='space-cadet cadet-underlined'>
-                                            American Gothic
-                                        </h4>
-                                    </a>
-                                </div>
-                                <div class='col-4'>
-                                    <a href='/artist-page.php' class='plain'>
-                                        <h4 class='card-title manatee'>Grant Wood</h4>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class='row ml-1'>
-                                <div class='col-6'>
-                                    <i class='bi bi-palette-fill manatee ml-1'></i>
-                                    <span class='space-cadet fs-6'>53 palettes</span>
-                                </div>
-                                <div class='col-6'>
-                                    <span class='imperial-red fs-6 bold'>Ksh. 30, 000</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
-                <div class='col-lg-4 col-md-6 col-sm-12 px-3 py-4'>
-                    <div class='card'>
-                        <img src="assets/art/grevy's-zebra-warhol.webp" class='card-img-top' alt="Grevy's Zebra Andy Warhol">
-                        <div class='card-body'>
-                            <div class='row ml-1'>
-                                <div class='col-5'>
-                                    <a href='/art/grevys-zebra.php'>
-                                        <h4 class='space-cadet cadet-underlined'>
-                                            Grevy's Zebra
-                                        </h4>
-                                    </a>
-                                </div>
-                                <div class='col-4'>
-                                    <a href='/artist-page.php' class='plain'>
-                                        <h4 class='card-title manatee'>Andy Warhol</h4>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class='row ml-1'>
-                                <div class='col-6'>
-                                    <i class='bi bi-palette-fill manatee ml-1'></i>
-                                    <span class='space-cadet fs-6'>109 palettes</span>
-                                </div>
-                                <div class='col-6'>
-                                    <span class='imperial-red fs-6 bold'>Ksh. 50, 000</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php if (isset($user)) : ?>
+                    My name is <?= $user['name'] ?>.
+                    My user id is <?= $user['id'] ?>.
+                <?php endif; ?>
 
-                <div class='col-lg-4 col-md-6 col-sm-12 px-3 py-4'>
-                    <div class='card'>
-                        <img src='assets/art/soup-cans-larger-warhol.jpg' class='card-img-top' alt='Soup Cans Andy Warhol'>
-                        <div class='card-body'>
-                            <div class='row ml-1'>
-                                <div class='col-4'>
-                                    <a href='/art/soup-cans-warhol.php'>
-                                        <h4 class='space-cadet cadet-underlined'>
-                                            Soup Cans
-                                        </h4>
-                                    </a>
-                                </div>
-                                <div class='col-4'>
-                                    <a href='/artist-page.php' class='plain'>
-                                        <h4 class='card-title manatee'>Andy Warhol</h4>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class='row'>
-                                <div class='col-6'>
-                                    <i class='bi bi-palette-fill manatee ml-1'></i>
-                                    <span class='space-cadet fs-6'>23 palettes</span>
-                                </div>
-                                <div class='col-6'>
-                                    <span class='imperial-red fs-6 bold'>Ksh. 100, 000</span>
+                <!-- Fetch all the art entries by the artist who is logged in -->
+                <!-- Render a card for each piece -->
+                <?php
+                $mysqli = require __DIR__ . '/database.php';
+
+                $fetch_stmt = "SELECT * FROM art";
+
+                $result = $mysqli->query($fetch_stmt);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $artist_id = $row['artist_id'];
+                        $piece_id = $row['id'];
+                        $piece_title = $row['title'];
+                        $artist_name = $row['artist_name'];
+                        $piece_story = $row['story'];
+                        $piece_price = $row['price'];
+                        $piece_dir = $row['img_path'];
+                ?>
+                        <div class='col-lg-4 col-md-6 col-sm-12 px-3 py-4'>
+                            <div class='card'>
+                                <img src='<?= $piece_dir ?>' class='card-img-top' alt='<? $piece_title ?>'>
+                                <div class='card-body'>
+                                    <div class='row ml-1'>
+                                        <div class='col-6'>
+                                            <a href='/art-piece-details.php?id=<?= $piece_id ?>'>
+                                                <h4 class='space-cadet cadet-underlined'><?= $piece_title ?></h4>
+                                            </a>
+                                        </div>
+                                        <div class='col-6'>
+                                            <a href='/artist-details.php?id=<?= $artist_id ?>' class='plain'>
+                                                <h4 class='card-title manatee'><?= $artist_name ?></h4>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class='row ml-1'>
+                                        <div class='col-6'>
+                                            <i class='bi bi-palette-fill manatee ml-1'></i>
+                                            <span class='space-cadet fs-6'>53 palettes</span>
+                                        </div>
+                                        <div class='col-6'>
+                                            <span class='imperial-red fs-6 bold'>Kshs <?= $piece_price ?></span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                <?php
+                    }
+                } else {
+                    echo "<center class='fs-5 mt-6 imperial-red'>No galleries found. Try uploading one. </center>";
+                }
+                ?>
             </div>
-
-
+            
             <div class='col-lg-12 col-md-12 col-sm-12 px-3 py-4'>
                 <div class='card'>
                     <div class='row'>
